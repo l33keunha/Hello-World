b5961a35ef84a4ce57f23fa96a3505bf4edfae81
l33ke <l33ke@DESKTOP-SQT86QJ>
2021-03-03 22:42:41 +0900

    첫 번째 Git Test

---
+++ 00_myFirstProject/src/com/kh/first/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.kh.first;
+
+public class HelloWorld { // 클래스
+	public static void main(String[] args) { // 메소드
+		System.out.println("Hello!");
+	}
+}
+// 주석 : 설명을 달기 위함, 컴파일이 되지 않음
+// 한줄주석	: 한 줄만 주석이 됨
+/*
+ * 여러 줄 주석
+ * 아래에 추가
+ * */
+ // ctrl + / 주석처리 단축키
